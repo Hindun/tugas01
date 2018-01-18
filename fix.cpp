c872fd8f7a848c57cc05777a1fc6aef049148e3c
hindun <hindunndunh@gmail.com>
2018-01-18 04:13:39 -0800

    Isi dari Class

--- fix.cpp
+++ fix.cpp
@@ -10,4 +10,23 @@ class jam{
 };
 
 int main(){
+	jam jamDinding,jamTangan,jamWeker;
+	
+	jamDinding.merk = "Bandul Sakana";
+	jamDinding.harga = 235000;
+	
+	jamTangan.merk = "Couple Casio";
+	jamTangan.harga= 760000;
+	
+	jamWeker.merk = "LED Digital Design Kayu alinsky";
+	jamWeker.harga = 288000;
+	
+	cout <<"Jam Dinding "<<(jamDinding.merk) <<" Harganya                 : Rp. "<<(jamDinding.harga)<<endl;
+	jamDinding.bersuara("sssssss");
+	
+	cout <<endl<<"Jam Tangan "<<(jamTangan.merk) <<" Harganya                   : Rp. "<<(jamTangan.harga)<<endl;
+	jamTangan.bersuara("tik tik tik");
+	
+	cout <<endl<<"Jam Weker "<< (jamWeker.merk) <<" Harganya : Rp. "<<(jamWeker.harga)<<endl;
+	jamWeker.bersuara("kriinggg");
 }
